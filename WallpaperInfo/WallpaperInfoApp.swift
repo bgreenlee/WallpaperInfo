033c919816b961a047a0c8128377e226b5700924
Brad Greenlee <brad@footle.org>
2024-11-04 16:01:37 -0800

    Initial Commit

---
+++ WallpaperInfo/WallpaperInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallpaperInfoApp.swift
+//  WallpaperInfo
+//
+//  Created by Brad Greenlee on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct WallpaperInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WallpaperInfoTests/WallpaperInfoTests.swift
@@ -0,0 +1,17 @@
+//
+//  WallpaperInfoTests.swift
+//  WallpaperInfoTests
+//
+//  Created by Brad Greenlee on 11/4/24.
+//
+
+import Testing
+@testable import WallpaperInfo
+
+struct WallpaperInfoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
